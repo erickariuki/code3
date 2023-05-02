e919d8c87087a26550c891db31b9a7bab8509e8b
erickariuki <eric.kariuki@student.moringaschool.com>
2023-05-02 09:49:41 +0300

    completed assignment

---
+++ code3/src/components/YourBotArmy.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Bot from './bot';
+function YourBotArmy({bots, handleClick, handleDischarge}) {
+  return (
+    <div>
+        <div className="row" style={{borderStyle: "solid", borderHeight: "300px", borderWidth: "300px"}}>
+            {bots.map(bot => <div className='col-4' style={{borderStyle: "solid"}}>
+                <Bot key={bot.id} bot={bot} onHandleDischarge={handleDischarge} onHandleClick={handleClick} />
+                </div>
+                )
+                }
+        </div>
+    </div>
+  );
+}
+export default YourBotArmy;
